ec2eab39b5d8b844d46525f1e5cc6171125b9cea
Matheus C. Proprentner <matheusproprentner@gmail.com>
2026-03-07 20:07:49 -0300

    feat: bean validation

--- src/main/java/org/coupon/api/domain/Coupon.java
+++ src/main/java/org/coupon/api/domain/Coupon.java
@@ -2,16 +2,17 @@
 
     import jakarta.persistence.Entity;
     import jakarta.persistence.GeneratedValue;
-    import jakarta.persistence.GenerationType;
     import jakarta.persistence.Id;
     import jakarta.persistence.Table;
+    import jakarta.validation.constraints.DecimalMin;
+    import jakarta.validation.constraints.NotNull;
+    import jakarta.validation.constraints.Size;
     import lombok.AllArgsConstructor;
     import lombok.Builder;
     import lombok.Data;
     import lombok.NoArgsConstructor;
     import org.coupon.api.exception.BusinessException;
     import org.hibernate.annotations.Where;
-
     import java.math.BigDecimal;
     import java.time.LocalDate;
     import java.util.UUID;
@@ -26,19 +27,25 @@
     public class Coupon {
 
         @Id
-        @GeneratedValue(strategy = GenerationType.UUID)
+        @GeneratedValue
         private UUID id;
 
+        @NotNull(message = "Code is required")
+        @Size(min = 6, max = 6, message = "Code must be exactly 6 characters after sanitization")
         private String code;
 
+        @NotNull(message = "Description is required")
+        @Size(min = 3, max = 255, message = "Description must be between 3 and 255 characters")
         private String description;
 
+        @NotNull(message = "Discount value is required")
+        @DecimalMin(value = "0.5", message = "Discount must be at least 0.5")
         private BigDecimal discountValue;
 
+        @NotNull(message = "Expiration date is required")
         private LocalDate expirationDate;
 
         private boolean published;
-
         private boolean deleted = false;
 
         public static Coupon create(
--- src/main/java/org/coupon/api/dto/CouponRequestDTO.java
+++ src/main/java/org/coupon/api/dto/CouponRequestDTO.java
@@ -1,12 +1,27 @@
 package org.coupon.api.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record CouponRequestDTO(
+        @NotNull(message = "Code is required")
+        @Size(min = 6, max = 6, message = "Code must be exactly 6 characters")
         String code,
+
+        @NotNull(message = "Description is required")
+        @Size(min = 3, max = 255, message = "Description must be between 3 and 255 characters")
         String description,
+
+        @NotNull(message = "Discount value is required")
+        @DecimalMin(value = "0.5", message = "Discount must be at least 0.5")
         BigDecimal discountValue,
+
+        @NotNull(message = "Expiration date is required")
         LocalDate expirationDate,
+
         boolean published
 ) {}
