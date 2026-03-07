b5767744e51448c26e06a45aaeafba9d13804da4
Matheus C. Proprentner <matheus.proprentner@wangaricarbon.com.br>
2026-03-07 16:41:46 -0300

    git initial

---
+++ src/main/java/org/coupon/api/CouponiApplication.java
@@ -0,0 +1,13 @@
+package org.coupon.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/coupon/api/CouponiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coupon.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
